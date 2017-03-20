c4fbca82f437d55fec5eb1fdd2ae724d2eedb186
Raghavendra Joshi <rjoshi@qwinix.io>
2017-03-20 18:16:41 +0530

    - Removed comment

--- lib/temperature_conversion_raghu.rb
+++ lib/temperature_conversion_raghu.rb
@@ -3,5 +3,4 @@ require 'temperature_conversion_raghu/centigrade_to_fahrenheit'
 require 'temperature_conversion_raghu/fahrenheit_to_centigrade'
 
 module TemperatureConversionRaghu
-  # Your code goes here...
 end
